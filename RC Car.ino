a59992dd79487131f187f5e83ca4249e370be48f
kayloleon <69911814+kayloleon@users.noreply.github.com>
2022-10-26 18:50:37 -0400

    Update RC Car.ino

--- RC Car.ino
+++ RC Car.ino
@@ -1,43 +1,44 @@
-char appInput;              //initializes char to be assigned
+char appInput;              // initializes char to be assigned
 
 void setup() {
- int leftMotorForward = 4; //left motors forward
- int leftMotorReverse = 5; //left motors reverse
- int rightMotorForward = 6; //right motors forward
- int rightMotorReverse = 7; //right motors reverse
+ int leftMotorForward = 4; // left motors forward
+ int leftMotorReverse = 5; // left motors reverse
+ int rightMotorForward = 6; // right motors forward
+ int rightMotorReverse = 7; // right motors reverse
  
  pinMode(leftMotorForward,OUTPUT);   
  pinMode(leftMotorReverse,OUTPUT);   
  pinMode(rightMotorForward,OUTPUT);   
  pinMode(rightMotorReverse,OUTPUT);   
- Serial.begin(9600);  //opens serial port at data rate
+ Serial.begin(9600);  // opens serial port at data rate
 }
  
-void loop() {            //loops through 'if' statements to look for input
+void loop() {            // loops through 'if' statements to look for input
  if(Serial.available()){
-   appInput = Serial.read();     //assigns char value from bluetooth app
+   appInput = Serial.read();     // assigns char value from bluetooth app
    Serial.println(appInput);
 }
  
-if(appInput == 'F'){            //turns on pins to rotate wheels forward
+if(appInput == 'F'){            // turns on pins to rotate wheels forward
   digitalWrite(leftMotorForward,HIGH);
   digitalWrite(rightMotorForward,HIGH);
 }
  
-else if(appInput == 'B'){      //turns on pins to rotate wheels backwards
+else if(appInput == 'B'){      // turns on pins to rotate wheels backwards
   digitalWrite(leftMotorReverses,HIGH);
   digitalWrite(rightMotorReverse,HIGH);
 }
  
-else if(appInput == 'L'){      //right motor turned on, turns left
+else if(appInput == 'L'){      // right motor turned on, turns left
   digitalWrite(rightMotorForward,HIGH);
 }
  
-else if(appInput == 'R'){      //left motor turned on, turns right
+else if(appInput == 'R'){      // left motor turned on, turns right
   digitalWrite(leftMotorForward,HIGH);
 }
  
-else if(appInput == 'S'){      //STOP, all motors off when no other input detected
+// STOP, all motors off when no other input detected 
+else if(appInput == 'S'){  
   digitalWrite(leftMotorForward,LOW);
   digitalWrite(leftMotorReverse,LOW);
   digitalWrite(rightMotorForward,LOW);
