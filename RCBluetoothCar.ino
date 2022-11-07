f79996d9cc45eadd4e14d03b1d936a4783b054a1
michaelm4320 <62119636+michaelm4320@users.noreply.github.com>
2022-11-07 14:24:29 -0500

    Update RCBluetoothCar.ino
    Correcting indentations

--- RCBluetoothCar.ino
+++ RCBluetoothCar.ino
@@ -6,48 +6,48 @@ int rightMotorForward = 6; // right motors forward
 int rightMotorReverse = 7; // right motors reverse
 
 void setup() {
- pinMode(leftMotorForward,OUTPUT);   
- pinMode(leftMotorReverse,OUTPUT);   
- pinMode(rightMotorForward,OUTPUT);   
- pinMode(rightMotorReverse,OUTPUT);   
- Serial.begin(9600);  // opens serial port at data rate
-}
- 
-void loop() {            // loops through 'if' statements to look for input
- if(Serial.available()){
-   appInput = Serial.read();     // assigns char value from bluetooth app
-   Serial.println(appInput);
-}
- 
-if(appInput == 'F'){            // turns on pins to rotate wheels forward
-  digitalWrite(leftMotorForward,HIGH);
-  digitalWrite(rightMotorForward,HIGH);
-}
- 
-else if(appInput == 'B'){      // turns on pins to rotate wheels backwards
-  digitalWrite(leftMotorReverse,HIGH);
-  digitalWrite(rightMotorReverse,HIGH);
-}
- 
-else if(appInput == 'L'){      // right motor turned on, turns left
-  digitalWrite(rightMotorForward,HIGH);
-}
- 
-else if(appInput == 'R'){      // left motor turned on, turns right
-  digitalWrite(leftMotorForward,HIGH);
-}
- 
-else if(appInput == 'X') {          // turn right motors on, doing a donut to the left
-  digitalWrite(rightMotorForward,HIGH);
-  delay(3000);
+	pinMode(leftMotorForward, OUTPUT);
+	pinMode(leftMotorReverse, OUTPUT);
+	pinMode(rightMotorForward, OUTPUT);
+	pinMode(rightMotorReverse, OUTPUT);
+	Serial.begin(9600);  // opens serial port at data rate
 }
 
-// STOP, all motors off when no other input detected 
-else if(appInput == 'S'){  
-  digitalWrite(leftMotorForward,LOW);
-  digitalWrite(leftMotorReverse,LOW);
-  digitalWrite(rightMotorForward,LOW);
-  digitalWrite(rightMotorReverse,LOW);
-}
-delay(100);
+void loop() {            // loops through 'if' statements to look for input
+	if (Serial.available()) {
+		appInput = Serial.read();     // assigns char value from bluetooth app
+		Serial.println(appInput);
+	}
+
+	if (appInput == 'F') {            // turns on pins to rotate wheels forward
+		digitalWrite(leftMotorForward, HIGH);
+		digitalWrite(rightMotorForward, HIGH);
+	}
+
+	else if (appInput == 'B') {      // turns on pins to rotate wheels backwards
+		digitalWrite(leftMotorReverse, HIGH);
+		digitalWrite(rightMotorReverse, HIGH);
+	}
+
+	else if (appInput == 'L') {      // right motor turned on, turns left
+		digitalWrite(rightMotorForward, HIGH);
+	}
+
+	else if (appInput == 'R') {      // left motor turned on, turns right
+		digitalWrite(leftMotorForward, HIGH);
+	}
+
+	else if (appInput == 'X') {          // turn right motors on, doing a donut to the left
+		digitalWrite(rightMotorForward, HIGH);
+		delay(3000);
+	}
+
+	// STOP, all motors off when no other input detected 
+	else if (appInput == 'S') {
+		digitalWrite(leftMotorForward, LOW);
+		digitalWrite(leftMotorReverse, LOW);
+		digitalWrite(rightMotorForward, LOW);
+		digitalWrite(rightMotorReverse, LOW);
+	}
+	delay(100);
 }
