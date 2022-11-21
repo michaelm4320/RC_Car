ee7b19b701c85e4a744e1286287f094494677e3a
michaelm4320 <62119636+michaelm4320@users.noreply.github.com>
2022-11-20 19:18:12 -0500

    avoid feature and new functions
    Added avoiding feature, added movement functions

--- RCBluetoothCar.ino
+++ RCBluetoothCar.ino
@@ -1,7 +1,7 @@
-#define echoPin 9 	// attach pin D9 Arduino to pin Echo of HC-SR04
-#define trigPin 8	// attach pin D8 Arduino to pin Echo of HC-SR04
+#define echoPin 9 // attach pin D9 Arduino to pin Echo of HC-SR04
+#define trigPin 8 // attach pin D8 Arduino to pin Echo of HC-SR04
 
-char appInput;         	// initializes char to be assigned
+char appInput;  // initializes char to be assigned
 
 int leftMotorForward = 5;  // left motors forward
 int leftMotorReverse = 4;  // left motors reverse
@@ -17,97 +17,125 @@ void setup() {
 	pinMode(rightMotorForward, OUTPUT);
 	pinMode(rightMotorReverse, OUTPUT);
 
-  	pinMode(trigPin, OUTPUT);	
-  	pinMode(echoPin, INPUT);
-
-	Serial.begin(9600);  // opens serial port at data rate
+	pinMode(trigPin, OUTPUT);
+	pinMode(echoPin, INPUT);
+	
+	// opens serial port at data rate
+	Serial.begin(9600);  
 }
 
 
-void forwards() {      // turns on pins to rotate wheels forward
-  	digitalWrite(leftMotorForward, HIGH);
-	digitalWrite(rightMotorForward, HIGH);
-}
-
-void backwards() {    // turns on pins to rotate wheels backwards
-  	digitalWrite(leftMotorReverse, HIGH);
-	digitalWrite(rightMotorReverse, HIGH);
-} 
-
-void left() {         // right motor turned on, turns left
-  	digitalWrite(rightMotorForward, HIGH);
-}
-
-void right() {        // left motor turned on, turns right
+void forwards() { // turns on pins to rotate wheels forward
 	digitalWrite(leftMotorForward, HIGH);
+	digitalWrite(rightMotorForward, HIGH);
+	digitalWrite(leftMotorReverse, LOW);
+	digitalWrite(rightMotorReverse, LOW);
 }
 
-void stop() {         // STOP, all motors off when no other input detected 
+
+void backwards() { // turns on pins to rotate wheels backwards
+	digitalWrite(leftMotorReverse, HIGH);
+	digitalWrite(rightMotorReverse, HIGH);
+	digitalWrite(leftMotorForward, LOW);
+	digitalWrite(rightMotorForward, LOW);
+}
+
+
+void left() { // right motor turned on, turns left
+	digitalWrite(rightMotorForward, HIGH);
+	digitalWrite(leftMotorForward, LOW);
+	digitalWrite(rightMotorReverse, LOW);
+	digitalWrite(leftMotorReverse, LOW);
+}
+
+
+
+void right() { // left motor turned on, turns right
+
+	digitalWrite(leftMotorForward, HIGH);
+	digitalWrite(leftMotorReverse, LOW);
+	digitalWrite(rightMotorForward, LOW);
+	digitalWrite(rightMotorReverse, LOW);
+}
+
+void stop() {  // STOP, all motors off when no other input detected 
 	digitalWrite(leftMotorForward, LOW);
 	digitalWrite(leftMotorReverse, LOW);
 	digitalWrite(rightMotorForward, LOW);
 	digitalWrite(rightMotorReverse, LOW);
 }
 
-void donut() {        // turn right motors on, doing a donut to the left        
+void donut() { // turn right motors on, doing a donut to the left        
 	digitalWrite(rightMotorForward, HIGH);
-	//delay(3000);
+	digitalWrite(leftMotorForward, LOW);
+	digitalWrite(rightMotorReverse, LOW);
+	digitalWrite(leftMotorReverse, LOW);
+	delay(3000);
 }
 
 int getDistance() {
 	// Clears the trigPin condition
-  	digitalWrite(trigPin, LOW);
-  	delayMicroseconds(2);
-  	// Sets the trigPin HIGH (ACTIVE) for 10 microseconds
- 	digitalWrite(trigPin, HIGH);
- 	delayMicroseconds(10);
-  	digitalWrite(trigPin, LOW);
-  	// Reads the echoPin, returns the sound wave travel time in microseconds
-  	duration = pulseIn(echoPin, HIGH);
-  	// Calculating the distance
-  	distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-  	// Displays the distance on the Serial Monitor
-  	Serial.print("Dist: ");
-  	Serial.print(distance);
-  	Serial.println(" cm");
-	
+	digitalWrite(trigPin, LOW);
+	delayMicroseconds(2);
+	// Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+	digitalWrite(trigPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trigPin, LOW);
+	// Reads the echoPin, returns the sound wave travel time in microseconds
+	duration = pulseIn(echoPin, HIGH);
+	// Calculating the distance
+	distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+	// Displays the distance on the Serial Monitor
+	Serial.print("Dist: ");
+	Serial.print(distance);
+	Serial.println(" cm");
+
 	return distance;
 }
 
 void avoid(int distance) {
 	stop();
-  	while (distance <= 30) {             	// backs up until distance is at least 40 cm
-    		backwards();
-  	}
+	delay(100);
+	// backs up until distance is at least 35 cm
+	while (getDistance() <= 35) {  
+		backwards();
+		delay(500);
+	}
+	// After backing up, car rotates right and then stops
 	right();
-  	delay(100); 
+	delay(1000);
+	stop();
 }
 
-void loop() {                       		// loops through 'if' statements to look for input
+// loops through 'if' statements to look for input
+void loop() {                       		
 	if (Serial.available()) {
-		appInput = Serial.read(); 	// assigns char value from bluetooth app
+		// assigns char value from bluetooth app
+		appInput = Serial.read(); 	
 		Serial.println(appInput);
 	}
-
-	distance = getDistance();         	// prints out and assigns detected distance
-
-  	if (distance <= 13) {             	// once distance is too small, avoid funtion is run
-    		avoid(distance);
-  	}
-
-	if (appInput == 'F') {           
-    		forwards();
+	
+	// prints out and assigns detected distance
+	distance = getDistance(); 
+	
+	// once distance is too small, avoid funtion is run
+	if (distance <= 20) { 
+		avoid(distance);
 	}
 
-	else if (appInput == 'B') {      
+	if (appInput == 'F') {
+		forwards();
+	}
+
+	else if (appInput == 'G') {
 		backwards();
 	}
 
-	else if (appInput == 'L') {      
+	else if (appInput == 'L') {
 		left();
 	}
 
-	else if (appInput == 'R') {     
+	else if (appInput == 'R') {
 		right();
 	}
 
@@ -120,5 +148,4 @@ void loop() {                       		// loops through 'if' statements to look f
 	}
 
 	delay(100);
-	
 }
