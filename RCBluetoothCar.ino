33893b941279ec42e84e1b7823b4199f4a290c2b
michaelm4320 <62119636+michaelm4320@users.noreply.github.com>
2022-11-27 19:24:40 -0500

    Update RCBluetoothCar.ino
    Added abstract classes

--- RCBluetoothCar.ino
+++ RCBluetoothCar.ino
@@ -1,147 +1,72 @@
-#define echoPin 9 // attach pin D9 Arduino to pin Echo of HC-SR04
-#define trigPin 8 // attach pin D8 Arduino to pin Echo of HC-SR04
+#ifndef base_h
+#define base_h
+#include "base.h"
+#endif
+#include "base_controls.h"
+
+
+base* myBase;
+base_controls myBaseCar;
 
 char appInput;  // initializes char to be assigned
-
-int leftMotorForward = 5;  // left motors forward
-int leftMotorReverse = 4;  // left motors reverse
-int rightMotorForward = 7; // right motors forward
-int rightMotorReverse = 6; // right motors reverse
 
 long duration;
 int distance;
 
+
 void setup() {
-	pinMode(leftMotorForward, OUTPUT);
-	pinMode(leftMotorReverse, OUTPUT);
-	pinMode(rightMotorForward, OUTPUT);
-	pinMode(rightMotorReverse, OUTPUT);
+  myBase = &myBaseCar;
+	pinMode(myBase->leftMotorForward, OUTPUT);
+	pinMode(myBase->leftMotorReverse, OUTPUT);
+	pinMode(myBase->rightMotorForward, OUTPUT);
+	pinMode(myBase->rightMotorReverse, OUTPUT);
 
-	pinMode(trigPin, OUTPUT);
-	pinMode(echoPin, INPUT);
-
+	pinMode(myBase->trigPin, OUTPUT);
+	pinMode(myBase->echoPin, INPUT);
+	
 	// opens serial port at data rate
-	Serial.begin(9600);
+	Serial.begin(9600);  
 }
 
-
-void forwards() { // turns on pins to rotate wheels forward
-	digitalWrite(leftMotorForward, HIGH);
-	digitalWrite(rightMotorForward, HIGH);
-	digitalWrite(leftMotorReverse, LOW);
-	digitalWrite(rightMotorReverse, LOW);
-}
-
-
-void backwards() { // turns on pins to rotate wheels backwards
-	digitalWrite(leftMotorReverse, HIGH);
-	digitalWrite(rightMotorReverse, HIGH);
-	digitalWrite(leftMotorForward, LOW);
-	digitalWrite(rightMotorForward, LOW);
-}
-
-
-void left() { // right motor turned on, turns left
-	digitalWrite(rightMotorForward, HIGH);
-	digitalWrite(leftMotorForward, LOW);
-	digitalWrite(rightMotorReverse, LOW);
-	digitalWrite(leftMotorReverse, LOW);
-}
-
-
-
-void right() { // left motor turned on, turns right
-
-	digitalWrite(leftMotorForward, HIGH);
-	digitalWrite(leftMotorReverse, LOW);
-	digitalWrite(rightMotorForward, LOW);
-	digitalWrite(rightMotorReverse, LOW);
-}
-
-void stop() {  // STOP, all motors off when no other input detected 
-	digitalWrite(leftMotorForward, LOW);
-	digitalWrite(leftMotorReverse, LOW);
-	digitalWrite(rightMotorForward, LOW);
-	digitalWrite(rightMotorReverse, LOW);
-}
-
-void donut() { // turn right motors on, doing a donut to the left        
-	left();
-	delay(3000);
-	stop();
-}
-
-int getDistance() {
-	// Clears the trigPin condition
-	digitalWrite(trigPin, LOW);
-	delayMicroseconds(2);
-	// Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-	digitalWrite(trigPin, HIGH);
-	delayMicroseconds(10);
-	digitalWrite(trigPin, LOW);
-	// Reads the echoPin, returns the sound wave travel time in microseconds
-	duration = pulseIn(echoPin, HIGH);
-	// Calculating the distance
-	distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-	// Displays the distance on the Serial Monitor
-	Serial.print(" Distance: ");
-	Serial.print(distance);
-
-	return distance;
-}
-
-void avoid(int distance) {
-	stop();
-	delay(100);
-	// backs up until distance is at least 35 cm
-	while (getDistance() <= 35) {
-		backwards();
-		delay(500);
-	}
-	// After backing up, car rotates right and then stops
-	right();
-	delay(1000);
-	stop();
-}
 
 // loops through 'if' statements to look for input
-void loop() {
+void loop() {                       		
 	if (Serial.available()) {
 		// assigns char value from bluetooth app
-		appInput = Serial.read();
+		appInput = Serial.read(); 	
 		Serial.println(appInput);
 	}
-
+	
 	// prints out and assigns detected distance
-	distance = getDistance();
-
+	distance = myBase->getDistance(); 
+	
 	// once distance is too small, avoid funtion is run
-	if (distance <= 20) {
-		avoid(distance);
+	if (distance <= 20) { 
+		myBase->avoid(distance);
 	}
 
 	if (appInput == 'F') {
-		forwards();
+		myBase->forwards();
 	}
 
 	else if (appInput == 'G') {
-		backwards();
+		myBase->backwards();
 	}
 
 	else if (appInput == 'L') {
-		left();
+		myBase->left();
 	}
 
 	else if (appInput == 'R') {
-		right();
+		myBase->right();
 	}
 
 	else if (appInput == 'X') {
-		donut();
+		myBase->donut();
 	}
 
 	else if (appInput == 'S') {
-		stop();
+		myBase->stop();
 	}
 
 	delay(100);
